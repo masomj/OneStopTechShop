87d726a68e42561b5380f608f82842d17e4fdc27
masomj <masonjayjones@gmail.com>
2022-12-08 22:38:20 +0000

    loaded data from fixtures into DB

--- OneStopTechShop/settings.py
+++ OneStopTechShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&e58!!4u&l^h$*wf1un)n=+q^!6_qgcaocl*_slv1($+&^i53r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'products'
 ]
 
 MIDDLEWARE = [
