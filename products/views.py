9a05a5402f741e246b11fbbfa3b0229d490375dc
masomj <masonjayjones@gmail.com>
2022-12-10 14:25:26 +0000

    created base template to display all products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse,get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
+
+def products(request):
+    products = Product.objects.all()
+    context={
+        'products':products
+    }
+    return render(request,'products.html',context)
