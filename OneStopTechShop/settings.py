74b2c8e2333449ab2933c4e31b351b2193bbde79
masomj <masonjayjones@gmail.com>
2022-12-11 16:10:47 +0000

    customised allauth login tempalate

--- OneStopTechShop/settings.py
+++ OneStopTechShop/settings.py
@@ -63,7 +63,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'templates')
+            os.path.join(BASE_DIR,'templates'),
+             os.path.join(BASE_DIR,'templates','allauth')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -147,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
