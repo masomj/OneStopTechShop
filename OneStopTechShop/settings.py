518ae47c0ac0ea3aaf71733f7dc4c555704511d3
Mason Jones <masonjayjones@gmail.com>
2022-12-16 15:58:21 +0000

    Created add to bag functionality

--- OneStopTechShop/settings.py
+++ OneStopTechShop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'home',
     'products',
+    'bag',
     'allauth', # new
     'allauth.account', # new
     'allauth.socialaccount', # new
@@ -73,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
