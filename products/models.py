010f373bf7447597d641df129f66f6aebc74051a
Mason Jones <masonjayjones@gmail.com>
2022-12-16 11:30:58 +0000

    Changed Product description model to 1024 char length

--- products/models.py
+++ products/models.py
@@ -2,6 +2,8 @@ from email.policy import default
 from unicodedata import category
 from django.db import models
 
+
+
 # Create your models here.
 class Category(models.Model):
     class Meta:
@@ -19,7 +21,6 @@ class CategoryParent(models.Model):
         return self.name
 
 class Product(models.Model):
-    
     title=models.CharField(max_length=254,null=True, blank=True)
     brand =models.CharField(max_length=254,null=True, blank=True)
     sku = models.CharField(max_length=254, null=True, blank=True)
@@ -27,12 +28,11 @@ class Product(models.Model):
     currency=models.CharField(max_length=254,null=True, blank=True)
     images=models.URLField(null=True,blank=True)
     category=models.ForeignKey('Category',null=True, blank=True, on_delete=models.SET_NULL)
-    description=models.CharField(max_length=254,null=True, blank=True)
+    description=models.CharField(max_length=1024, null=True, blank=True)
     average_rating=models.CharField(max_length=254,null=True, blank=True)
     reviews_count= models.IntegerField(null=True, blank=True)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    
-    
+
     def __str__(self):
         return self.title
     
