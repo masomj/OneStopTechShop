d918d1ea4ee4f8aa5a7f5bf41d2228b91a765f3d
Mason Jones <masonjayjones@gmail.com>
2022-12-16 11:31:31 +0000

    created edit page functionality

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.products, name='products'),
-path('<product_id>', views.productInfo, name='productInfo')
+path('<product_id>', views.productInfo, name='productInfo'),
+path('<product_id>/',views.editProduct, name='editProduct'),
 ]
