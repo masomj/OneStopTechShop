ed308e51d03ee3d62a1ad0ed1ff5e340491f65b7
Mason Jones <masonjayjones@gmail.com>
2022-12-29 15:15:59 +0000

    Added update quantity functionality to bag view

--- bag/views.py
+++ bag/views.py
@@ -28,4 +28,16 @@ def add_to_bag(request, product_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+def update_bag(request, product_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag',{})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] = quantity
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
+
+
 
