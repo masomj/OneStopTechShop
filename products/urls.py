d78d4ab85a3e508e209ba0bc479e5945e827aab3
Mason Jones <masonjayjones@gmail.com>
2022-12-30 14:59:39 +0000

    add admin template

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('', views.products, name='products'),
-path('<product_id>', views.productInfo, name='productInfo'),
-path('<product_id>/',views.editProduct, name='editProduct'),
-path('<product_id>/delete',views.deleteProduct, name='deleteProduct'),
-path('/createProduct',views.createProduct,name='createProduct')
+path('', views.products, name='products'), #products
+path('<product_id>', views.productInfo, name='productInfo'), #product info
+path('admin/editprodict/<product_id>',views.editProduct, name='editProduct'), #edit product info
+path('admin/delete/<product_id>',views.deleteProduct, name='deleteProduct'), #delete product
+path('admin/createProduct',views.createProduct,name='createProduct'), #create new product
+path('admin/',views.admin ,name="admin") #view admin page
 ]
