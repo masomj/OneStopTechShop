a55899049dddf7003e4aabfe28fa68b6ac2e01d1
masomj <masonjayjones@gmail.com>
2023-01-02 15:02:23 +0000

    create view for profile details

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import userDetails
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
-
 def profile(request):
-    return render(request, 'profile.html')
+    logged_in_user = request.user.id
+    userdetails = get_object_or_404(userDetails, pk=logged_in_user)
+    print(logged_in_user)
+    
+    context={
+        'user':logged_in_user,
+        'userdetails':userdetails,
+    }
+    print(userdetails.postcode)
+    return render(request, 'profile.html',context)
