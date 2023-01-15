b1df6ed0a22543c1fba4672d43f12fb9c927287b
masomj <masonjayjones@gmail.com>
2023-01-15 11:04:08 +0000

    added basic stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from userprofile.models import userDetails
 from django.contrib.auth.models import User
 from django.conf import settings
 from bag.contexts import bag_contents
+from .models import order, orderItem
 import stripe
+
 
 
 # Create your views here
@@ -38,14 +40,45 @@ def pay(request):
     stripe.api_key = private
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
-        currency = settings.STRIPE_CURRENCY,
+        currency = settings.STRIPE_CURRENCY,)
+    
+    
+    if request.method == 'POST':
+        user_details ={
+            
+        }
+        
+        
+        bag = request.session.get('bag',{})
+        for item_id, item_data in bag.items():
+            try:
+                product=Product.objects.get(id=item_id)
+                if isinstance(item_data, int):
+                    order_item = orderItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data
+                    )
+                    order_item.save()
+                
+    
+                    
+            except Product.DoesNotExist:
+                print('Not found')
+                order.delete()
+                
+                return redirect(reverse('view_bag'))
+                    
     
         
-    )
+        
+    
     print(intent)
     context={
         'stripe_public_key':public,
         'client_secret':intent.client_secret
     }
+    
+    
     return render(request,'stripe_checkout.html', context)
     
