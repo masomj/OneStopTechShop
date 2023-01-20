3a86b7df8c325762abbdd7dcdb4a5a04e49ef793
masomj <masonjayjones@gmail.com>
2023-01-20 21:55:50 +0000

    changes to imports

--- OneStopTechShop/settings.py
+++ OneStopTechShop/settings.py
@@ -19,6 +19,10 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import django
+from django.utils.encoding import force_str
+django.utils.encoding.force_text = force_str
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
