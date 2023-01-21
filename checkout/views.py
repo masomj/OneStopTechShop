ee4fed7545a7f4db535e4e3e652a9ecdf61e3223
masomj <masonjayjones@gmail.com>
2023-01-21 14:58:53 +0000

    fix 404 error if user ha no saved details

--- checkout/views.py
+++ checkout/views.py
@@ -15,7 +15,7 @@ def checkout(request):
     if request.user.is_authenticated:
         logged_in_user = request.user.id
         userObj = get_object_or_404(User, pk=logged_in_user)
-        userdetails = get_object_or_404(userDetails, user_id=userObj.id)
+        userdetails = userDetails.objects.filter(user_id=userObj)
         context ={
         'userdetails':userdetails
         }
