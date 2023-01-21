20e69f80a00ff77b50c9ec0558c76812761aaf60
masomj <masonjayjones@gmail.com>
2023-01-21 14:59:20 +0000

    add semicolons

--- static/js/select_quantity.js
+++ static/js/select_quantity.js
@@ -1,27 +1,27 @@
 incrementBtn = document.getElementById('increment-qty');
 decrementBtn = document.getElementById('decrement-qty');
 function handleIncrement(evnt){
-    evnt.preventDefault()
-    qty = parseInt(document.getElementById('id-qty').value)
+    evnt.preventDefault();
+    qty = parseInt(document.getElementById('id-qty').value);
     qty++;
-    handleValueChange(qty)
+    handleValueChange(qty);
 }
 
 function handleDecrement(evnt){
-    evnt.preventDefault()
-    qty = parseInt(document.getElementById('id-qty').value)
+    evnt.preventDefault();
+    qty = parseInt(document.getElementById('id-qty').value);
     --qty;
-    handleValueChange(qty)
+    handleValueChange(qty);
 }
 
 function handleValueChange(qty){
     if (qty >= 1 && qty <= 99){
-        document.getElementById('id-qty').value = qty
+        document.getElementById('id-qty').value = qty;
     } else{
         evnt.target.disabled=true;
     } 
 }
 
-incrementBtn.addEventListener('click',handleIncrement)
-decrementBtn.addEventListener('click',handleDecrement)
+incrementBtn.addEventListener('click',handleIncrement);
+decrementBtn.addEventListener('click',handleDecrement);
 
